bcdbac74ee161e6bd027e4459cee4b944fd1bd23
3yd <pereponov_alexander@mail.ru>
2024-09-16 00:44:29 +0300

    Added some checks

--- src/test/java/homework/PracticeFormTest.java
+++ src/test/java/homework/PracticeFormTest.java
@@ -1,4 +1,6 @@
 package homework;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,14 +8,28 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Alexander");
+        $("#lastName").setValue("3yd");
+        $("#userEmail").setValue("3yd@corp.com");
+        $("[class=custom-control-label]").click();
+        $("#userNumber").setValue("9169169169");
         $("").setValue("");
         $("").setValue("");
         $("").setValue("");
         $("").setValue("");
-        $("").setValue("");
+        $("#currentAddress").setValue("Some street 1");
         $("").setValue("");
         $("").setValue("");
         $("").click();
